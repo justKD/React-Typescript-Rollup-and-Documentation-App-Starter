7b376cdc8d36351ade0935230e018729044dbbee
justKD <cady.holmes.dev@gmail.com>
2021-02-24 16:52:01 -0600

    fixed rollup ui buttons responsive styling

--- src/dev/rollup/index.js
+++ src/dev/rollup/index.js
@@ -3,18 +3,22 @@ import { bundle } from './bundle';
 const entry = './src/dev/build/index.js';
 const output = 'bundle.js';
 
+const buttonWidth = 150;
+
 const bundleControls = document.createElement('div');
 bundleControls.id = 'bundle-controls';
-bundleControls.style = 'margin-top: 60px; margin-left: 100px;';
+bundleControls.style =
+  'display: flex; width: 100%; align-items: center; justify-content: flex-end; margin-top: 20px;';
 
 const copyButton = document.createElement('input');
 copyButton.id = 'bundle-log';
-copyButton.style = 'margin-right: 10px;';
+copyButton.style = `width: ${buttonWidth}px; margin-right: 10px; margin-left: 60px;`;
 copyButton.type = 'button';
 copyButton.value = 'Copy Bundle';
 
 const downloadButton = document.createElement('input');
 downloadButton.id = 'bundle-dl';
+downloadButton.style = `width: ${buttonWidth}px; margin-left: 10px; margin-right: 10px;`;
 downloadButton.type = 'button';
 downloadButton.value = 'Download Bundle';
 
